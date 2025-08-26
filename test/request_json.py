2f3c9521efb11a6e60ecce20977b5a1498860c85
Frank Qiu <shengqiu.purdue@gmail.com>
2025-08-26 15:10:56 +0800

    change structure

--- test/request_json.py
+++ test/request_json.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 
 url = "https://ec.sinopec.com/f/supp/bid/queryInquiryotices.do?paramJson=%7B%7D"
@@ -10,5 +11,6 @@ headers = {
   'Cookie': 'JSESSIONID=1A8BDFEDD38F947419EC2E86D17489FA; SERVERID=7c87ef910d16daea198140372adca059|1756190655|1756190655; seid=1A8BDFEDD38F947419EC2E86D17489FA; sevIp=247',
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
 }
-response = requests.request("POST", url, headers=headers, data=payload)
-print(response.text)
+response = requests.post(url, headers=headers, data=payload)
+data = json.loads(response.text)['result']['result']
+print(data)
