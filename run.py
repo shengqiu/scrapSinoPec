ee115152e9eedc2a4dc55a4f78486c278cfb3cd5
Frank Qiu <shengqiu.purdue@gmail.com>
2025-08-26 17:43:45 +0800

    successfully get winner of bids

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
 from util.url import config, get_listing_from_index, get_webpage_index
-from util.parse import get_index_list_from_index_response
+from util.parse import get_index_list_from_content, get_winner_from_response, get_date_from_response, get_contact_from_response
 import time
 
 if __name__ == "__main__":
     page_no = config['payload']['pageNo']
     index_response = get_webpage_index(page_no)
-    index_list = get_index_list_from_index_response(index_response)
+    index_list = get_index_list_from_content(index_response)
     for index in index_list:
         print(index)
-        print(get_listing_from_index(index))
+        listing_response = get_listing_from_index(index)
+        winner = get_winner_from_response(listing_response)
+        contact = get_contact_from_response(listing_response)
+        dates = get_date_from_response(listing_response)
+        print(dates)
+        print(winner)
+        print(contact)
         time.sleep(1)
--- util/parse.py
+++ util/parse.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup as bs
 import json
 
 
-def get_index_list_from_index_response(response):
+def get_index_list_from_content(response):
     """
     Extracts a list of indices from the HTML content of a webpage.
     Args:
@@ -14,3 +14,42 @@ def get_index_list_from_index_response(response):
     id_list = [x['id'] for x in data]
     return id_list
 
+
+def get_winner_from_response(response):
+    """
+    Extracts a table from the HTML content of a webpage.
+    Args:
+        html_content (str): The HTML content of the webpage.
+    Returns:
+        bs4.element.Tag: The extracted table as a BeautifulSoup Tag object.
+    """
+    soup = bs(response.text, 'html.parser')
+    table = soup.find_all('table')[2]
+    return table.get_text(strip=True).replace('物资名称成交供应商', ' ')
+
+
+def get_contact_from_response(response):
+    """
+    Extracts a table from the HTML content of a webpage.
+    Args:
+        html_content (str): The HTML content of the webpage.
+    Returns:
+        bs4.element.Tag: The extracted table as a BeautifulSoup Tag object.
+    """
+    soup = bs(response.text, 'html.parser')
+    table = soup.find_all('table')[3]
+    return table.get_text(strip=True)
+
+
+def get_date_from_response(response):
+    """
+    Extracts the date from the HTML content of a webpage.
+    Args:
+        html_content (str): The HTML content of the webpage.
+    Returns:
+        str: The extracted date as a string.
+    """
+    soup = bs(response.text, 'html.parser')
+    date_elements = soup.find('div', class_='wrapTitle').find_all('b')
+    data_elements_text = ' '.join([x.get_text(strip=True) for x in date_elements])
+    return data_elements_text
