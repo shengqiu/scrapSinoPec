a1a12985e6cdf3aeb9d2d5f053a16bb5741dc318
Frank Qiu <shengqiu.purdue@gmail.com>
2025-08-26 15:14:04 +0800

    now add more

--- run.py
+++ run.py
@@ -3,8 +3,6 @@ from util.url import download_webpage
 
 
 if __name__ == "__main__":
-    num1 = sys.argv[1]
-    num2 = sys.argv[2]
-    for num in [num1, num2]:
-        url_to_download = "https://ec.sinopec.com/f/supp/notice/bidNotice.do?id=" + str(num)
-        download_webpage(url_to_download, "webpage/{}.html".format(num))
+    num = sys.argv[1]
+    url_to_download = "https://ec.sinopec.com/f/supp/notice/bidNotice.do?id=" + str(num)
+    download_webpage(url_to_download, "webpage/{}.html".format(num))
