b06531b1cbb9b23b3e9d275267887876ce011ba6
Frank Qiu <shengqiu.purdue@gmail.com>
2025-08-28 11:28:09 +0800

    add

--- test.py
+++
@@ -1,8 +0,0 @@
-#! /Users/ff/Projects/scrapSinoPec/.conda/bin/python 
-
-import requests, yaml
-from util.url import get_bidding_index
-config = yaml.safe_load(open("config/sinoPec.yaml"))
-
-if __name__ == "__main__":
-    get_bidding_index("1", "openType")
